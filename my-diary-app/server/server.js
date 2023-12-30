c3f97bd521ebbcfb9deef8c77ef104cf19dfee62
ewoo14 <ewoo2821@gmail.com>
2023-12-30 13:46:37 +0900

    서버 실행 시 데이터베이스 자동 생성

--- my-diary-app/server/server.js
+++ my-diary-app/server/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql2');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
+const crypto = require('crypto');
 
 // 타임스탬프 함수
 const moment = require('moment-timezone');
@@ -10,36 +11,101 @@ function getCurrentTimestamp() {
     return moment().tz('Asia/Seoul').format('YYYY-MM-DD HH:mm:ss');
 }
 
-// 로그를 기록 함수
+// 로그를 기록하는 함수
 function logAction(email, message) {
     console.log(`[${getCurrentTimestamp()}] [${email}] ${message}`);
 }
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+// 암호화 키
+const ENCRYPTION_KEY = 'your-32-byte-long-encryption-key'; // 32바이트의 키
+const IV_LENGTH = 16; // 초기화 벡터 길이
 
-const db = mysql.createConnection({
+// 텍스트 암호화 함수
+function encrypt(text) {
+    let iv = crypto.randomBytes(IV_LENGTH);
+    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return iv.toString('hex') + ':' + encrypted.toString('hex');
+}
+
+// 텍스트 복호화 함수
+function decrypt(text) {
+    let textParts = text.split(':');
+    let iv = Buffer.from(textParts.shift(), 'hex');
+    let encryptedText = Buffer.from(textParts.join(':'), 'hex');
+    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+}
+
+// 데이터베이스 연결 설정
+const dbConfig = {
     host: 'localhost',
     user: 'root',
-    password: 'Angelic1440!',
-    database: 'diaryDB'
-});
+    password: 'Angelic1440!'
+};
 
-// 앱 서버가 정상적으로 가동 중인지 확인하는 로직
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+// MySQL 데이터베이스 연결
+const db = mysql.createConnection(dbConfig);
 
-db.connect(err => {
-    if (err) {
-        throw err;
-    }
-    console.log('MySQL successfully connected!');
-});
+// 서버 초기화 및 데이터베이스 설정
+function initializeServer() {
+  // 데이터베이스 연결
+  db.connect(err => {
+      if (err) {
+          throw err;
+      }
+      console.log('MySQL successfully connected!');
 
-// 회원가입 라우트
-app.post('/userregister', async (req, res) => {
+      // diaryDB 데이터베이스 생성
+      db.query("CREATE DATABASE IF NOT EXISTS diaryDB", (err, result) => {
+          if (err) throw err;
+          console.log("Database created or already exists");
+
+          // diaryDB 사용 설정
+          db.changeUser({database: 'diaryDB'}, (err) => {
+              if (err) throw err;
+
+              // users 테이블 생성
+              const createUsersTable = `
+                  CREATE TABLE IF NOT EXISTS users (
+                      id INT AUTO_INCREMENT PRIMARY KEY,
+                      email VARCHAR(255) NOT NULL UNIQUE,
+                      password VARCHAR(255) NOT NULL,
+                      name VARCHAR(100) NOT NULL,
+                      phone VARCHAR(20) NOT NULL
+                  )`;
+              db.query(createUsersTable, (err, result) => {
+                  if (err) throw err;
+                  console.log("Users table created or already exists");
+
+                  // diaries 테이블 생성
+                  const createDiariesTable = `
+                      CREATE TABLE IF NOT EXISTS diaries (
+                          diary_id INT AUTO_INCREMENT PRIMARY KEY,
+                          user_id INT,
+                          date DATE,
+                          content TEXT,
+                          FOREIGN KEY (user_id) REFERENCES users(id)
+                      )`;
+                  db.query(createDiariesTable, (err, result) => {
+                      if (err) throw err;
+                      console.log("Diaries table created or already exists");
+                  });
+              });
+          });
+      });
+  });
+
+  // Express 앱 설정
+  const app = express();
+  app.use(cors());
+  app.use(bodyParser.json());
+
+  // 회원가입 라우트
+  app.post('/userregister', async (req, res) => {
     try {
         const { email, password, name, phone } = req.body;
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -56,10 +122,10 @@ app.post('/userregister', async (req, res) => {
         logAction(email, `Server error on registering: ${err.message}`);
         res.status(500).send({ message: 'Server error' });
     }
-});
+  });
 
-// 이메일 중복 확인 라우트
-app.get('/check-email/:email', (req, res) => {
+  // 이메일 중복 확인 라우트
+  app.get('/check-email/:email', (req, res) => {
     const emailToCheck = req.params.email;
   
     db.query('SELECT * FROM users WHERE email = ?', [emailToCheck], (err, results) => {
@@ -78,10 +144,10 @@ app.get('/check-email/:email', (req, res) => {
       logAction(emailToCheck, 'Email available');
       res.status(200).send({ message: '사용 가능한 이메일입니다.' });
     });
-});
+  });
 
-// 로그인 라우트
-app.post('/userlogin', (req, res) => {
+  // 로그인 라우트
+  app.post('/userlogin', (req, res) => {
     const { email, password } = req.body;
 
     // 사용자 이메일로 데이터베이스 조회
@@ -100,35 +166,35 @@ app.post('/userlogin', (req, res) => {
             res.status(401).send({ message: 'Login error' });
         }
     });
-});
-
-// 사용자 이름 조회 라우트
-app.get('/username', (req, res) => {
-  const userId = req.query.userId;
-  logAction(userId, `Username request received: ${userId}`);
-  if (!userId) {
-    logAction(userId, `Username request error: No user ID provided`);
-    return res.status(400).send({ message: 'No user ID provided' });
-  }
-
-  db.query('SELECT name FROM users WHERE id = ?', [userId], (err, results) => {
-    if (err) {
-        logAction(userId, `Username request error: ${err.message}`);
-        return res.status(500).send({ message: 'Server error' });
-    }
-
-    // 결과가 비어있는 경우 처리
-    if (results.length === 0) {
-        logAction(userId, `Username request error: User not found`);
-        return res.status(404).send({ message: 'User not found' });
-    }
-
-    res.send({ name: results[0].name });
   });
-});
 
-// 일기저장 라우트
-app.post('/savediary', (req, res) => {
+  // 사용자 이름 조회 라우트
+  app.get('/username', (req, res) => {
+    const userId = req.query.userId;
+    logAction(userId, `Username request received: ${userId}`);
+    if (!userId) {
+      logAction(userId, `Username request error: No user ID provided`);
+      return res.status(400).send({ message: 'No user ID provided' });
+    }
+  
+    db.query('SELECT name FROM users WHERE id = ?', [userId], (err, results) => {
+      if (err) {
+          logAction(userId, `Username request error: ${err.message}`);
+          return res.status(500).send({ message: 'Server error' });
+      }
+  
+      // 결과가 비어있는 경우 처리
+      if (results.length === 0) {
+          logAction(userId, `Username request error: User not found`);
+          return res.status(404).send({ message: 'User not found' });
+      }
+  
+      res.send({ name: results[0].name });
+    });
+  });
+
+  // 일기저장 라우트
+  app.post('/savediary', (req, res) => {
     const { user_id, date, content } = req.body;
     const encryptedContent = encrypt(content);
   
@@ -166,8 +232,8 @@ app.post('/savediary', (req, res) => {
     });
   });
 
-// 특정 날짜의 일기 조회 라우트
-app.get('/diary/:date', (req, res) => {
+  // 특정 날짜의 일기 조회 라우트
+  app.get('/diary/:date', (req, res) => {
     const userId = req.query.userId;
     const date = req.params.date;
   
@@ -196,33 +262,13 @@ app.get('/diary/:date', (req, res) => {
       const decryptedContent = decrypt(results[0].content);
     res.send({ content: decryptedContent });
     });
-});
+  });
 
-// 보안 함수
-const crypto = require('crypto');
-
-// 암호화 키
-const ENCRYPTION_KEY = 'your-32-byte-long-encryption-key'; // 32바이트의 키
-const IV_LENGTH = 16; // 초기화 벡터 길이
-
-function encrypt(text) {
-    let iv = crypto.randomBytes(IV_LENGTH);
-    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
-    let encrypted = cipher.update(text);
-
-    encrypted = Buffer.concat([encrypted, cipher.final()]);
-
-    return iv.toString('hex') + ':' + encrypted.toString('hex');
+  // 서버 시작
+  app.listen(3000, () => {
+      console.log('Server is running on port 3000');
+  });
 }
 
-function decrypt(text) {
-    let textParts = text.split(':');
-    let iv = Buffer.from(textParts.shift(), 'hex');
-    let encryptedText = Buffer.from(textParts.join(':'), 'hex');
-    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
-    let decrypted = decipher.update(encryptedText);
-
-    decrypted = Buffer.concat([decrypted, decipher.final()]);
-
-    return decrypted.toString();
-}
+// 서버 초기화 실행
+initializeServer();
